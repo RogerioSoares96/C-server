f3ff3d4ec564883aa9b9b1c8181911eacdd0543b
Rogerio Soares <rogeryu_gap@hotmail.com>
2023-04-27 19:28:52 +0100

    Added Main as the client side connection handler

---
+++ src/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <netinet/in.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Incorrect Number of Arguments\n");
+        return 1;
+    }
+    int network_socket = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in socket_address;
+    socket_address.sin_family = AF_INET;
+    socket_address.sin_port = (atoi(argv[1]));
+    socket_address.sin_addr.s_addr = INADDR_ANY;
+
+    int connection = connect(network_socket, (struct sockaddr *) &socket_address, sizeof(socket_address));
+    if (connection == -1) {
+        printf("Connection unsuccessful - Client Side\n");
+        return 2;
+    }
+
+    // recv(network_socket, )
+    return 0;
+}
