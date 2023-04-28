7f9535c8561731fae6861ef122d939f7f7549650
Rogerio Soares <rogeryu_gap@hotmail.com>
2023-04-28 15:51:58 +0100

    Fixed nameing convention on client side of TCP server

--- src/main.c
+++ src/main.c
@@ -11,20 +11,20 @@ int main(int argc, char *argv[]) {
         printf("Incorrect Number of Arguments\n");
         return 1;
     }
-    int network_socket = socket(AF_INET, SOCK_STREAM, 0);
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in socket_address;
     socket_address.sin_family = AF_INET;
     socket_address.sin_port = (atoi(argv[1]));
     socket_address.sin_addr.s_addr = INADDR_ANY;
 
-    int connection = connect(network_socket, (struct sockaddr *) &socket_address, sizeof(socket_address));
+    int connection = connect(client_socket, (struct sockaddr *) &socket_address, sizeof(socket_address));
     if (connection == -1) {
         printf("Connection unsuccessful - Client Side\n");
         return 2;
     }
     char buffer[3000];
-    recv(network_socket, &buffer, sizeof(buffer), 0);
+    recv(client_socket, &buffer, sizeof(buffer), 0);
     printf("%s", buffer);
-    close(network_socket);
+    close(client_socket);
     return 0;
 }
